7db644b1929cce21c0f2b3ca769e16d250b82eaa
Dennis Costa <56238317+fcdennis@users.noreply.github.com>
2020-08-15 13:12:56 -0300

    addField

--- Rocketseat/nextLevelWeek2/script/addField.js
+++ Rocketseat/nextLevelWeek2/script/addField.js
@@ -1,9 +1,12 @@
-//Procurar o botão
-document.querySelector('#add-time')
-.addEventListener('click', cloneField)
-//Quando clicar no botão
+document.querySelector("#add-time").addEventListener('click', cloneField)
 
-//Executar uma ação
 function cloneField() {
-    
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+    const fields = newFieldContainer.querySelectorAll('input')
+
+    fields.forEach(function(field){
+        field.value = ""
+    })
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+
 }
