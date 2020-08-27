519a606762e7c99ae6b5779d85663380f131f84e
Dennis Costa <56238317+fcdennis@users.noreply.github.com>
2020-08-27 19:22:27 -0300

    simplificando funções

--- Curso_em_Vídeo/aula15/teste019.js
+++ Curso_em_Vídeo/aula15/teste019.js
@@ -1,25 +1,41 @@
-function parImpar (n) {
-    if (n % 2 == 0){
+const parOuImpar = n => n % 2 === 0 ? 'Par' : 'Impar'
+console.log(`O valor da função com 1 param é ${parOuImpar(6)}`)
+/*
+A função acima é a mesma coisa que:
+
+function parOuImpar(n) {
+    if (n % 2 === 0) {
         return 'Par'
-    }
-    else {
+    } else {
         return 'Impar'
     }
 }
-teste = parImpar(6)
-console.log(`O valor da função com 1 param é ${teste}`)
+console.log(`O valor da função com 1 param é ${parOuImpar(6)}`)
+*/
 
-function somar (n1=0, n2=0) {
+let somar = (n1=0, n2=0) => n1 + n2
+console.log(`O valor da função com 2 param é ${somar(5, 6)}`)
+/*
+A função acima é a mesma coisa que:
+
+function somar(n1=0, n2=0) {
     return n1 + n2
 }
-teste = somar(5, 6)
-console.log(`O valor da função com 2 param é ${teste}`)
+console.log(`O valor da função com 2 param é ${somar(5, 6)}`)
+*/
+
+
+const fatorial = n => n ===1 ? 1 : n * fatorial(n-1)
+console.log(`O valor da função recursiva é ${fatorial(5)}`)
+/*
+A função acima é a mesma coisa que:
 
 function fatorial(n){
-    if (n==1){
+    if (n === 1) {
         return 1
-    } else{
-        return n * fatorial(n-1)
+    } else {
+        return n * fatorial(n - 1)
     }
 }
 console.log(`O valor da função recursiva é ${fatorial(5)}`)
+*/
